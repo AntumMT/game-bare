97d390c3040548059c13e334f84d4965ced16359
Jordan Irwin <antumdeluge@gmail.com>
2022-09-09 16:14:31 -0700

    Add mod to provide generic ground & water tiles

---
+++ mods/ground/init.lua
@@ -0,0 +1,41 @@
+
+if not core.settings:get_bool("empty_game_enable_ground", false) then
+  do return end
+end
+
+
+core.register_node("ground:dirt", {
+  description = "Dirt",
+  tiles = {"ground_dirt.png"},
+  groups = {crumbly=3},
+})
+
+core.register_node("ground:stone", {
+  description = "Stone",
+  tiles = {"ground_stone.png"},
+  groups = {cracky=3},
+})
+
+core.register_node("ground:water", {
+  description = "Water",
+  tiles = {"ground_water.png"},
+  groups = {water=3, liquid=3},
+  paramtype = "light",
+  drawtype = "liquid",
+  use_texture_alpha = "blend",
+  walkable = false,
+  pointable = false,
+  diggable = false,
+  buildable_to = true,
+  is_ground_content = false,
+  drop = "",
+  liquidtype = "source",
+  liquid_viscosity = 1,
+  liquid_alternative_flowing = "ground:water",
+  liquid_alternative_source = "ground:water",
+})
+
+
+core.register_alias("mapgen_dirt", "ground:dirt")
+core.register_alias("mapgen_stone", "ground:stone")
+core.register_alias("mapgen_water_source", "ground:water")
